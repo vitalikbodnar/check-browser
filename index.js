b939e7384635747dbf7523275439c1d823adfd4b
Vitalik Bodnar <vitalii.bodnar@otakoyi.com>
2020-01-15 10:40:20 +0200

    changes

--- index.js
+++
@@ -1,8 +0,0 @@
-// import checkbrowser from "dist/checkbrowser"
-//
-// $(document).ready(() => {
-//     checkbrowser();
-// });
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-}
